95b561fd87ab386680ef02e74db504effc67d309
Edgar Montiel Cruz <xrnoz@outlook.com>
2023-02-22 13:19:30 -0500

    Better support for heroicons mini variant
    Made the vue component more like the one from vuetify/iconsets/mdi-svg.
    
    Updated dependencies.

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { mergeProps, createVNode, toRefs, JSXComponent } from 'vue';
-import { defineComponent, propsFactory } from 'vuetify/lib/util/index.mjs';
-import { IconValue } from 'vuetify/lib/composables/icons.mjs';
+import { mergeProps, createVNode, JSXComponent } from 'vue';
+import { defineComponent } from 'vuetify/lib/util/index.mjs';
+import { makeIconProps } from 'vuetify/lib/composables/icons.mjs';
 
 import type { IconDefinition } from '@fortawesome/fontawesome-common-types';
 import type { IconProps, IconSet } from 'vuetify';
@@ -13,9 +13,9 @@ export const embedIcon = (icon: string) => icon;
 export const mdiEmbed = embedIcon;
 export const mdilEmbed = embedIcon;
 
-export const hisToString = (icon: string) => `SVG;0 0 24 24;${icon};;;evenodd`;
-export const hioToString = (icon: string) => `SVG;0 0 24 24;${icon};none;1.5`;
-export const himToString = (icon: string) => `SVG;0 0 20 20;${icon}`;
+export const hisToString = (icon: string) => `SVG;;${icon};;;evenodd`;
+export const hioToString = (icon: string) => `SVG;;${icon};none;1.5`;
+export const himToString = (icon: string) => `SVG;0 0 20 20;${icon};;;evenodd`;
 export const hisEmbed = hisToString;
 export const hioEmbed = hioToString;
 export const himEmbed = himToString;
@@ -23,24 +23,9 @@ export const himEmbed = himToString;
 export const SVGIcon: JSXComponent<IconProps> = defineComponent({
   name: 'SVGIcon',
   inheritAttrs: false,
-  props: propsFactory(
-    {
-      icon: {
-        type: IconValue,
-        required: true,
-      },
-      tag: {
-        type: String,
-        required: true,
-      },
-    },
-    'icon',
-  )(),
-  setup(props: IconProps, _ref: { attrs: Record<string, unknown> }) {
-    const { icon, tag } = toRefs(props);
-    const { attrs } = _ref;
-
-    const svgProps: Record<string, any> = {
+  props: makeIconProps(),
+  setup(props: IconProps, { attrs }: any) {
+    const svgAttrs: Record<string, any> = {
       class: 'v-icon__svg',
       xmlns: 'http://www.w3.org/2000/svg',
       viewBox: '0 0 24 24',
@@ -48,38 +33,32 @@ export const SVGIcon: JSXComponent<IconProps> = defineComponent({
       'aria-hidden': 'true',
     };
 
-    let d = icon?.value as string;
+    let d = props.icon as string;
 
     if (d.startsWith?.('SVG;')) {
       const parts = d.split(';');
-      svgProps['viewBox'] = parts[1] ?? '';
+
       d = parts[2] ?? '';
 
-      if (parts[3] && parts[3].length > 0) {
-        svgProps['fill'] = parts[3];
-        svgProps['style'] = `fill: ${parts[3]};`;
+      if (parts[1]?.length) svgAttrs['viewBox'] = parts[1];
+
+      if (parts[3]?.length) {
+        svgAttrs['fill'] = parts[3];
+        svgAttrs['style'] = `fill: ${parts[3]};`;
       }
 
-      if (parts[4] && parts[4].length > 0) {
-        svgProps['stroke-width'] = parts[4];
-        svgProps['stroke'] = 'currentColor';
+      if (parts[4]?.length) {
+        svgAttrs['stroke-width'] = parts[4];
+        svgAttrs['stroke'] = 'currentColor';
       }
 
-      if (parts[5] && parts[5].length > 0) {
-        svgProps['fill-rule'] = parts[5];
-      }
+      if (parts[5]?.length) svgAttrs['fill-rule'] = parts[5];
     }
 
     return () => {
-      return createVNode(
-        tag.value,
-        mergeProps(attrs, {
-          style: null,
-        }),
-        {
-          default: () => [createVNode('svg', svgProps, [createVNode('path', { d }, null)])],
-        },
-      );
+      return createVNode(props.tag, mergeProps(attrs, { style: null }), {
+        default: () => [createVNode('svg', svgAttrs, [createVNode('path', { d }, null)])],
+      });
     };
   },
 });
