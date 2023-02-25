4b411d439572c0be4e5a39a2c79d0a504b055753
Edgar Montiel Cruz <xrnoz@outlook.com>
2023-02-25 15:19:55 -0500

    Fixed component to re-render when the icon changes

--- index.ts
+++ index.ts
@@ -25,37 +25,37 @@ export const SVGIcon: JSXComponent<IconProps> = defineComponent({
   inheritAttrs: false,
   props: makeIconProps(),
   setup(props: IconProps, { attrs }: any) {
-    const svgAttrs: Record<string, any> = {
-      class: 'v-icon__svg',
-      xmlns: 'http://www.w3.org/2000/svg',
-      viewBox: '0 0 24 24',
-      role: 'img',
-      'aria-hidden': 'true',
-    };
-
-    let d = props.icon as string;
-
-    if (d.startsWith?.('SVG;')) {
-      const parts = d.split(';');
-
-      d = parts[2] ?? '';
-
-      if (parts[1]?.length) svgAttrs['viewBox'] = parts[1];
-
-      if (parts[3]?.length) {
-        svgAttrs['fill'] = parts[3];
-        svgAttrs['style'] = `fill: ${parts[3]};`;
-      }
-
-      if (parts[4]?.length) {
-        svgAttrs['stroke-width'] = parts[4];
-        svgAttrs['stroke'] = 'currentColor';
-      }
-
-      if (parts[5]?.length) svgAttrs['fill-rule'] = parts[5];
-    }
-
     return () => {
+      const svgAttrs: Record<string, any> = {
+        class: 'v-icon__svg',
+        xmlns: 'http://www.w3.org/2000/svg',
+        viewBox: '0 0 24 24',
+        role: 'img',
+        'aria-hidden': 'true',
+      };
+
+      let d = props.icon as string;
+
+      if (d.startsWith?.('SVG;')) {
+        const parts = d.split(';');
+
+        d = parts[2] ?? '';
+
+        if (parts[1]?.length) svgAttrs['viewBox'] = parts[1];
+
+        if (parts[3]?.length) {
+          svgAttrs['fill'] = parts[3];
+          svgAttrs['style'] = `fill: ${parts[3]};`;
+        }
+
+        if (parts[4]?.length) {
+          svgAttrs['stroke-width'] = parts[4];
+          svgAttrs['stroke'] = 'currentColor';
+        }
+
+        if (parts[5]?.length) svgAttrs['fill-rule'] = parts[5];
+      }
+
       return createVNode(props.tag, mergeProps(attrs, { style: null }), {
         default: () => [createVNode('svg', svgAttrs, [createVNode('path', { d }, null)])],
       });
